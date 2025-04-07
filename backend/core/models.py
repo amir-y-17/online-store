7b7bc950a27a2768980745c35aef6c8a83d16253
Amir <am17iryaghoobi@gmail.com>
2025-04-08 00:43:53 +0330

    create PasswordReset model

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class PasswordReset(models.Model):
+    email = models.EmailField()
+    token = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
