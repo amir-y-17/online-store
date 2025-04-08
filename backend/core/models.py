9fd791b693889997559342c7b855af794a885310
Amir <am17iryaghoobi@gmail.com>
2025-04-08 14:36:34 +0330

    delete reset password model

--- backend/core/models.py
+++ backend/core/models.py
@@ -8,9 +8,3 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
-
-
-class PasswordReset(models.Model):
-    email = models.EmailField()
-    token = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
