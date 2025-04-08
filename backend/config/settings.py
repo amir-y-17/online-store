696c16d4b090a5852b918fe9f67c4588157b8928
Amir <am17iryaghoobi@gmail.com>
2025-04-08 14:19:49 +0330

    add email sending settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -161,3 +161,11 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
 }
+
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "tiamtestblog@gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = Config("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
