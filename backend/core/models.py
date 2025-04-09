f1678f177b49692434fdce8ceb8a796c41ff53c7
Amir <am17iryaghoobi@gmail.com>
2025-04-09 13:14:19 +0330

    update user model fields

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,8 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    address = models.TextField()
-    profile_picture = models.ImageField(upload_to="profile_pictures/")
+    address = models.TextField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/", null=True, blank=True
+    )
 
     def __str__(self):
         return self.username
