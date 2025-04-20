9f77f232e323f618e78220d12dd91fc7eb6b1f1d
shahin <shahin.chehreh@gmail.com>
2025-04-20 15:14:58 +0330

    come onnnn

--- frontend/src/pages/Register/Register.js
+++ frontend/src/pages/Register/Register.js
@@ -1,5 +1,4 @@
-import React, { useEffect } from "react";
-import { data, Link } from "react-router-dom";
+import {Link } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
 import "./Register.css";
 
