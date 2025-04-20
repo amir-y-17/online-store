3ba022f872e11e1c24710ce89d95ff661b68d7b4
shahin <shahin.chehreh@gmail.com>
2025-04-20 16:44:55 +0330

    change backend setting For connect to frontend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -63,7 +63,9 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+
+
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
